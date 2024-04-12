ee4a76916c2e6689522e6f0a8d135b9fcbe15153
aurelien <aurelien.blot@adg-software.fr>
2024-04-12 13:33:01 +0200

    first commit

---
+++ src/main/java/com/castruche/cast_games_messages/CastGamesMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.castruche.cast_games_messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastGamesMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastGamesMessagesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/castruche/cast_games_messages/CastGamesMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castruche.cast_games_messages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastGamesMessagesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
