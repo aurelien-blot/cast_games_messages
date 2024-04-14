12d262ffcf1446165b2980cfb446a0b0b7dcc0f3
Castruche <aurelien.blot@gmail.com>
2024-04-14 21:42:28 +0200

    Mise en place rabbit

--- src/main/java/com/castruche/cast_games_messages/service/MessageReceptionService.java
+++ src/main/java/com/castruche/cast_games_messages/service/MessageReceptionService.java
@@ -9,14 +9,22 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MessageReceptionService {
 
     private static final Logger logger = LogManager.getLogger(MessageReceptionService.class);
 
+    private MessageService messageService;
+
+    public MessageReceptionService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     public void receiveMessage(MessageDto message) {
         logger.info("Message received: " + message);
     }
-
 
 }
